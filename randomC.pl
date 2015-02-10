03e39a8f448cdc1a762e3a2289d221745f7cdcb8
rzhang84 <rzhan001@fiu.edu>
2015-02-10 01:00:01 -0600

    algorithms
    ex delete self loop

--- randomC.pl
+++ randomC.pl
@@ -11,32 +11,31 @@ while(<KM>){
 	$vertice{shift @number}=\@number;
 }
 close KM;
-my $i=0; ##keep
-my $j=0; ##to delete
+my $i=1; ##keep
+my $j=1; ##to delete
 my @k=(); ##to collect
 
-for (my $n=0;$n<200;$n++){##random
+for (my $n=0;$n<199;$n++){##random
     $i=int(rand(200))+1;
     $j=int(rand(200))+1;
-    while (grep {$_ eq $i} @k){
+    while (grep {$_ == $i} @k){
         $i=int(rand(200))+1;  	        }
-    while(grep {$_ eq $j} @k || $i==$j){
+    while( (grep {$_ == $j} @k) || ($i==$j) ){
         $j=int(rand(200))+1;  
     }
     
     push @k, $j;
     print "$i $j \n";
     ##merge two vertice
-#    @{$vertice{$i}}=( @{$vertice{$i}},@{$vertice{$j}});
-#    delete $vertice{$j};
- #   my %merge=();
- #   $merge{$i}=$j;
+@{$vertice{$i}}=( @{$vertice{$i}},@{$vertice{$j}});
+delete $vertice{$j};
 
 ###delete self loop
 #    @{$vertice{$i}}= grep { 
 #	$_!= $merge{$i};
 #	} @{$vertice{$i}};
 	
-###count
-#    print "@{$vertice{$i}} \n";
 }
+
+###count
+print "@{$vertice{$i}} \n";
