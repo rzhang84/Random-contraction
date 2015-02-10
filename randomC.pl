86cf28f56113e34dc1a3e7b82f21ffa4c7676c18
rzhang84 <rzhan001@fiu.edu>
2015-02-10 00:31:50 -0600

    algorithms
    variables

--- randomC.pl
+++ randomC.pl
@@ -11,23 +11,34 @@ while(<KM>){
 	$vertice{shift @number}=\@number;
 }
 close KM;
-my $i=1;
-my $j=4;
-
-##random $i=int(rand(200));
-$j=int(rand(200));
+my $i=int(rand(200))+1; ##keep
+my $j=int(rand(200))+1; ##to delete
 while($i==$j){
-    $j=int(rand(200));		}
-print "$i $j \n";
-##merge two vertice
-@{$vertice{$i}}=( @{$vertice{$i}},@{$vertice{$j}});
-delete $vertice{$j};
-my %merge=();
-$merge{$i}=$j;
+    $j=int(rand(200))+1;		
+} 
+my @k=(); ##to collect
+
+for (my $n=0;$n<200;$n++){##random
+    while (grep {$_ eq $i || $_ eq $j} @k){
+        $i=int(rand(200));
+        $j=int(rand(200));
+        while($i==$j){
+            $j=int(rand(200));		
+        }  	        }
+    
+    push @k, $j;
+    print "$i $j \n";
+    ##merge two vertice
+#    @{$vertice{$i}}=( @{$vertice{$i}},@{$vertice{$j}});
+#    delete $vertice{$j};
+ #   my %merge=();
+ #   $merge{$i}=$j;
 
 ###delete self loop
-@{$vertice{$i}}= grep { 
-	$_!= merge{$i};
-	} @{$vertice{$i}};
+#    @{$vertice{$i}}= grep { 
+#	$_!= $merge{$i};
+#	} @{$vertice{$i}};
 	
 ###count
+#    print "@{$vertice{$i}} \n";
+}
